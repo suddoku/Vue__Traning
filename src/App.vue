77a06dad1f45cd541a9f2f0ba35155d78d489c92
suddoku <kirill.shemyagov@yahoo.com>
2022-04-16 22:17:32 +0300

    First__change

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <FirstTraning/>
+  </div>
+</template>
+
+<script>
+import FirstTraning from "@/components/FirstTraning";
+
+export default {
+  name: 'App',
+  components: {
+    FirstTraning
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/FirstTraning.vue
@@ -0,0 +1,62 @@
+<template>
+<div class="first__frame">
+  <h1>
+    <a v-bind:href="url"><img src="../assets/googlelogo_color_272x92dp.png"></a>
+    <p> {{ Number(sto) + Number(inp) }} </p>
+  </h1>
+  <input v-model="inp">
+</div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'FirstTraning',
+  data () {
+    return {
+      url: 'https://www.google.com',
+      sto: 100,
+      inp: ''
+    }
+  }
+}
+
+</script>
+
+<style lang="css" scoped>
+.first__frame {
+  display: grid;
+  justify-content: center;
+}
+h1 {
+  color: lightslategray;
+  font-size: 46px;
+}
+
+a {
+  text-decoration: none;
+  color: lightslategray;
+  margin-bottom: 30px;
+}
+
+input {
+  padding: 0px 30px;
+  border-radius: 24px;
+  border: 1px solid #e3e3e3;
+  background-color: #fff;
+  transition: all 0.1s;
+  width: 540px;
+  height: 46px;
+}
+
+input:hover {
+  box-shadow: 0 1px 6px rgb(32 33 36 / 28%);
+}
+
+input:focus-visible {
+  outline: none;
+  box-shadow: 0 1px 6px rgb(32 33 36 / 28%)
+}
+
+</style>
